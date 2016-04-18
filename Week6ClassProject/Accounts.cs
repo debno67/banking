88eb9617631fc1c4ba9af765a747fcdb33a461a6
Your Name <you@example.com>
2016-04-18 09:26:32 -0400

    submission

---
+++ Week6ClassProject/Accounts.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6ClassProject
+{
+    class Account
+    {
+        //accounts class
+        //each transaction with date and time (Date & Time Class)
+        //+ for deposit 
+        //- for withdraw
+        //show transaction amount
+        //show new balance
+        //Streamwriter and account summary
+        //public void AccountInfo()
+
+        //fields
+
+        double balance;
+        string firstName;
+        string lastName;
+
+        //properties
+
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+        //withdraw method
+        public void Withdraw()
+        {
+            Console.WriteLine("Withdrew amount ${0}");
+            double amount = double.Parse(Console.ReadLine());
+            this.balance -= amount;
+        }
+        //deposit method
+        public void Deposit()
+        {
+            Console.WriteLine("Deposit amount ${0}");
+            double amount = double.Parse(Console.ReadLine());
+            this.balance += amount;
+
+        }
+        //balance method
+        public void GetBalance()
+        {
+            Console.WriteLine("Balance, {0}", this.balance);
+        }
+
+        //constructor
+        public Account()
+        {
+        }
+        public Account(double balance, string firstName, string lastName)
+        {
+
+            this.Balance = balance;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
---
+++ Week6ClassProject/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6ClassProject
+{
+    class Client
+    {
+        //client class
+        //client name
+        //client acct number
+        //random method to generate account number
+        //fields
+        private string name = "Lexus Davis";
+        private int accountNumber = int.Parse("123445678 ");
+       
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+        //methods
+
+        public void Userinfo()
+        {
+
+            Console.WriteLine(this.name + " Account Number: " + this.accountNumber);
+        }
+        public Client()
+        {
+
+        }
+        public Client(string name, int accountNumber)
+        {
+            this.AccountNumber = accountNumber;
+            this.Name = name;
+        }
+    }
+}
+
